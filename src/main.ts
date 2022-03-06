3bac582448fad295ee8dc0a4f6bb60bb9f9a5bad
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-03-07 00:10:18 +0900

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
+    credentials: true,
+  });
   await app.listen(4040);
 }
 
